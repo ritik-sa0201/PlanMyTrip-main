462f37747f1101acdc66e39aa18b6fd8f6ea757b
psaini0809 <145288512+psaini0809@users.noreply.github.com>
2026-07-08 03:50:27 +0530

    fix api

--- server/app/main.py
+++ server/app/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://plan-my-trip-main-omega.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
